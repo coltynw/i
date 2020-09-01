7dcedf6906dccb858324b82e627c800afb0d0376
Coltyn2 <coltynwiewel@gmail.com>
2020-09-01 16:16:10 -0400

    Updates

---
+++ precache-manifest.e2062ab85549d26da9d67210a5641998.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0526c1f807f14aaa86c34a167a8ef035",
+    "url": "/coltynw/i/index.html"
+  },
+  {
+    "revision": "a7a918656cff17328d39",
+    "url": "/coltynw/i/static/css/main.c1b0d2a4.chunk.css"
+  },
+  {
+    "revision": "baa7ffb9f9373db5be19",
+    "url": "/coltynw/i/static/js/2.96b5aa38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coltynw/i/static/js/2.96b5aa38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a918656cff17328d39",
+    "url": "/coltynw/i/static/js/main.8d872ab0.chunk.js"
+  },
+  {
+    "revision": "f2b7e548719dac00463d",
+    "url": "/coltynw/i/static/js/runtime-main.0f2f8b80.js"
+  },
+  {
+    "revision": "4d8847acfdbc37d94e3147c56ceaae84",
+    "url": "/coltynw/i/static/media/91RKJ6KC62PA1594365571485.4d8847ac.jpg"
+  },
+  {
+    "revision": "87084765c971b0b84a93003ede465dba",
+    "url": "/coltynw/i/static/media/Abomination.87084765.png"
+  },
+  {
+    "revision": "2f9f175ba488eda1064239744ac0b975",
+    "url": "/coltynw/i/static/media/AncientAftershock.2f9f175b.png"
+  },
+  {
+    "revision": "f404ff318b4df303e7143f0f6615dbe1",
+    "url": "/coltynw/i/static/media/AshenHallow.f404ff31.png"
+  },
+  {
+    "revision": "21b5fa12fc80e838c9f7399758e3f741",
+    "url": "/coltynw/i/static/media/Blessing.21b5fa12.png"
+  },
+  {
+    "revision": "5bd2d78537cf0a13753ed4b428768901",
+    "url": "/coltynw/i/static/media/BonedustBrew.5bd2d785.png"
+  },
+  {
+    "revision": "147c1f3fc49c9c0930689519cc240427",
+    "url": "/coltynw/i/static/media/Boon.147c1f3f.png"
+  },
+  {
+    "revision": "c1cf4930df93032ac7a1730480d32980",
+    "url": "/coltynw/i/static/media/ChainHarvest.c1cf4930.png"
+  },
+  {
+    "revision": "c7bca7f00425433b017592c032d87a74",
+    "url": "/coltynw/i/static/media/Condemn.c7bca7f0.png"
+  },
+  {
+    "revision": "63c5cb4e165e9923de6df62fd9abd18c",
+    "url": "/coltynw/i/static/media/ConquerorsBanner.63c5cb4e.png"
+  },
+  {
+    "revision": "528fdf34ed3e0a72ca2bdc965b19e4c9",
+    "url": "/coltynw/i/static/media/Convoke.528fdf34.png"
+  },
+  {
+    "revision": "fa3fae4e095cc45ceccd91404a285168",
+    "url": "/coltynw/i/static/media/DeathChakram.fa3fae4e.png"
+  },
+  {
+    "revision": "696a000a853f4026b76e920bab3fa73d",
+    "url": "/coltynw/i/static/media/Deathborne.696a000a.png"
+  },
+  {
+    "revision": "2f660d5fba92d22789e65800df5cf028",
+    "url": "/coltynw/i/static/media/DeathsDue.2f660d5f.png"
+  },
+  {
+    "revision": "dbfe4f3e1284f4b3b89770ec89f4a29f",
+    "url": "/coltynw/i/static/media/DecimatingBolt.dbfe4f3e.png"
+  },
+  {
+    "revision": "33ede2c7186c40eac3a3e7ae448d4f02",
+    "url": "/coltynw/i/static/media/DivineToll.33ede2c7.png"
+  },
+  {
+    "revision": "2de960437f02d7b7c60f8f1cc7771bb1",
+    "url": "/coltynw/i/static/media/EchoingReprimand.2de96043.png"
+  },
+  {
+    "revision": "1cbe7ee8d4c753916dffc4eb7e7380dc",
+    "url": "/coltynw/i/static/media/Elysian.1cbe7ee8.png"
+  },
+  {
+    "revision": "d043e9c14941be3840583f71af2a6e94",
+    "url": "/coltynw/i/static/media/FaeGuardians.d043e9c1.png"
+  },
+  {
+    "revision": "dc88905cc3d3f6666349028dec99c6ec",
+    "url": "/coltynw/i/static/media/FaeTransfusion.dc88905c.png"
+  },
+  {
+    "revision": "406dbda0695d1c1f1d22a6066f4455b2",
+    "url": "/coltynw/i/static/media/FaelineStomp.406dbda0.png"
+  },
+  {
+    "revision": "8adad4aa1021f60f0ab0f1649d62a206",
+    "url": "/coltynw/i/static/media/FallenOrder.8adad4aa.png"
+  },
+  {
+    "revision": "c9cde7e0733457867605395f6682490e",
+    "url": "/coltynw/i/static/media/Flayed.c9cde7e0.png"
+  },
+  {
+    "revision": "2ca48add117ff4775a0ffb832ff04387",
+    "url": "/coltynw/i/static/media/Fodder.2ca48add.png"
+  },
+  {
+    "revision": "b1f3f680e9dc7ee4e6cb6f88ff635508",
+    "url": "/coltynw/i/static/media/ImpendingCatastrophe.b1f3f680.png"
+  },
+  {
+    "revision": "78c6ecf2f8cba7acf25d0b8ceae685ac",
+    "url": "/coltynw/i/static/media/Kindred.78c6ecf2.png"
+  },
+  {
+    "revision": "1ceb25e9984cc91ad4c4a790bd3edd94",
+    "url": "/coltynw/i/static/media/Mindgames.1ceb25e9.png"
+  },
+  {
+    "revision": "aedf53d024103153c6e599c043476471",
+    "url": "/coltynw/i/static/media/Mirror.aedf53d0.png"
+  },
+  {
+    "revision": "d7cd9e86530a1a548e859b2711c25702",
+    "url": "/coltynw/i/static/media/PrimordialWave.d7cd9e86.png"
+  },
+  {
+    "revision": "5ee957e759fa272e4ae55e8d1c6f6c4b",
+    "url": "/coltynw/i/static/media/Ravenous.5ee957e7.png"
+  },
+  {
+    "revision": "036a7b0ce18d227db85578e09972d54f",
+    "url": "/coltynw/i/static/media/Resonating.036a7b0c.png"
+  },
+  {
+    "revision": "04092b8ab3bf16bf9cc730aecb89a4eb",
+    "url": "/coltynw/i/static/media/ScouringTithe.04092b8a.png"
+  },
+  {
+    "revision": "674701bec43ec06069b562a02d2468e2",
+    "url": "/coltynw/i/static/media/Sepsis.674701be.png"
+  },
+  {
+    "revision": "2a60e852102f4a99356be4481ee1c846",
+    "url": "/coltynw/i/static/media/SerratedBoneSpike.2a60e852.png"
+  },
+  {
+    "revision": "7893180074b87a298ba368712d6a80a4",
+    "url": "/coltynw/i/static/media/Shackle.78931800.png"
+  },
+  {
+    "revision": "719a545f02c8ac871ab5a48abe8d24d0",
+    "url": "/coltynw/i/static/media/ShiftingPower.719a545f.png"
+  },
+  {
+    "revision": "3dedb6caf7c11d4f096fc78a59899fed",
+    "url": "/coltynw/i/static/media/SinfulBrand.3dedb6ca.png"
+  },
+  {
+    "revision": "0f884ef0c6156b41d53e34bead55c385",
+    "url": "/coltynw/i/static/media/Slaghter.0f884ef0.png"
+  },
+  {
+    "revision": "cbf537e2e2949c55ac384f9f57307b82",
+    "url": "/coltynw/i/static/media/SoulRot.cbf537e2.png"
+  },
+  {
+    "revision": "230f64b98737b94d5be6fc66b9fc88e5",
+    "url": "/coltynw/i/static/media/Spark.230f64b9.png"
+  },
+  {
+    "revision": "a7fc1b7cc987ab279c86f7bc9bc93057",
+    "url": "/coltynw/i/static/media/SpearOfBastion.a7fc1b7c.png"
+  },
+  {
+    "revision": "d317bfd941ea5703146fc697dad46fb6",
+    "url": "/coltynw/i/static/media/Swarm.d317bfd9.png"
+  },
+  {
+    "revision": "60ea90eccbfefd8b69a7c8de359ba6db",
+    "url": "/coltynw/i/static/media/SwarmingMist.60ea90ec.png"
+  },
+  {
+    "revision": "2e02be72c34299ea779ee90b5ce9d96b",
+    "url": "/coltynw/i/static/media/TheHunt.2e02be72.png"
+  },
+  {
+    "revision": "7461af0273d7bcc1b88ed29032900a0c",
+    "url": "/coltynw/i/static/media/UnholyNova.7461af02.png"
+  },
+  {
+    "revision": "33c63ffef0253fc0be7cbb05bef8021e",
+    "url": "/coltynw/i/static/media/VanquishersHammer.33c63ffe.png"
+  },
+  {
+    "revision": "b2e9e75c8afedd8d326f90f94b913549",
+    "url": "/coltynw/i/static/media/VesperTotem.b2e9e75c.png"
+  },
+  {
+    "revision": "95e8a6d7a9da1e1f13db222fb52b7225",
+    "url": "/coltynw/i/static/media/WeaponsOfOrder.95e8a6d7.png"
+  },
+  {
+    "revision": "3aeb8072bba211903922f1257535f8d2",
+    "url": "/coltynw/i/static/media/WildSpirits.3aeb8072.png"
+  }
+]);
